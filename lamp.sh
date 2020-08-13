bcf6a6a5b978cf75972768e7a4409688ea367631
RaviDeva <69040149+RaviDeva@users.noreply.github.com>
2020-08-13 08:53:55 +0200

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -2,58 +2,56 @@
 apt-get -y update
 
 # set up a silent install of MySQL
-dbpass=$1
+# dbpass=$1
 
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+# export DEBIAN_FRONTEND=noninteractive
+# echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+# echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # install the LAMP stack
 apt-get -y install apache2 mysql-server php5 php5-mysql unzip
 
 
 
-sudo su
+#sudo su
 
-mkdir tmp
+#mkdir tmp
 
-cd tmp
+cd /tmp
 
 git init
 
 git clone "https://github.com/santoshreddyspy13/terraform"
 
-cd terraform
+cd /tmp/terraform
 
 
+#mysqladmin create armdatabase
+
+mysql -uroot armdatabase < redlog.sql
 
 
-mysqladmin create armdatabase
+#mkdir file
 
-mysql armdatabase < redlog.sql
+#cp redlog.zip file
 
-
-mkdir file
-
-cp redlog.zip file
-
-cd file
+#cd file
 
 unzip redlog.zip
 
-cd redlog
+cd /tmp/terraform/redlog/
 
 cp -r * /var/www/html/
 
-cd ..
+# cd ..
 
-rm -rf file
+# rm -rf file
 
-cd ..
+# cd ..
 
-cd ..
+# cd ..
 
-rm -rf tmp
+#rm -rf tmp
 
 
 # restart Apache
